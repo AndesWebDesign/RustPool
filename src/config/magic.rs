d9b79dc9bf744fdbb73543a4886afacbb249fe43
cbh <cbharris@andeswebdesign.com>
2021-06-14 11:30:04 -0500

    add new rpc request logic

--- src/config/magic.rs
+++ src/config/magic.rs
@@ -8,3 +8,5 @@ pub const JSON_RPC_VERSION: &str = "2.0";
 pub const MESSAGE_BUFFER_SIZE: usize = 1024;
 pub const RUSTPOOL_LOG_TIME_FORMAT_STRING: &str = "%Y-%m-%dT%H:%M:%S%.3f";
 pub const ENVIRONMENT_VARIABLE_PREFIX: &str = "RUSTPOOL";
+pub const WWW_AUTH_HEADER_NAME: &str = "WWW-Authenticate";
+pub const AUTH_HEADER_NAME: &str = "Authorization";
--- src/stream/rpc.rs
+++ src/stream/rpc.rs
@@ -1,36 +1,95 @@
-use log::{info, warn};
-use serde_json::{from_str, from_value, json, Value};
-use digest_auth::AuthContext;
-use reqwest::{Client, StatusCode, Response};
-use crate::config::magic::{JSON_RPC_VERSION, MISSING_STRING};
-use crate::structs::{PayoutDTO, BlockTemplate, Config, TransferResponse};
+use std::str::{from_utf8, FromStr};
 
-async fn make_daemon_rpc_request(config: &Config, body: Value) -> Option<Value> {
+use digest_auth::{AuthContext, AuthorizationHeader, Error, WwwAuthenticateHeader};
+use log::{debug, info, warn};
+use reqwest::{Client, Response, StatusCode};
+use reqwest::header::HeaderName;
+use serde_json::{from_str, from_value, json, Value, Map};
+
+use crate::config::magic::{AUTH_HEADER_NAME, JSON_RPC_VERSION, MISSING_STRING, WWW_AUTH_HEADER_NAME};
+use crate::structs::{BlockTemplate, Config, PayoutDTO, TransferResponse};
+
+async fn make_daemon_rpc_request(config: &Config, body: &Value) -> Option<Value> {
     return make_rpc_request(body,
                             &config.daemon_rpc_url,
                             &config.daemon_rpc_user,
-                            &config.daemon_rpc_password).await;
+                            &config.daemon_rpc_password,
+                            None).await;
 }
 
-async fn make_wallet_rpc_request(config: &Config, body: Value) -> Option<Value> {
+async fn make_wallet_rpc_request(config: &Config, body: &Value) -> Option<Value> {
     return make_rpc_request(body,
                             &config.wallet_rpc_url,
                             &config.wallet_rpc_user,
-                            &config.wallet_rpc_password).await;
+                            &config.wallet_rpc_password,
+                            None).await;
 }
 
-async fn make_rpc_request(body: Value, uri: &String, username: &String, password: &String) -> Option<Value> {
-    let mut context = AuthContext::new(username, password, uri);
+fn parse_www_auth_header(response: Response, context: AuthContext) -> Option<AuthorizationHeader> {
+    let resp_headers = response.headers();
+    println!("{:?}", resp_headers);
+    if !resp_headers.contains_key(WWW_AUTH_HEADER_NAME) {
+        return None;
+    }
+    let www_auth_header = resp_headers.get(WWW_AUTH_HEADER_NAME).unwrap();
+    match digest_auth::parse(www_auth_header.to_str().unwrap_or(MISSING_STRING)) {
+        Ok(mut www_auth_header) => {
+            match AuthorizationHeader::from_prompt(&mut www_auth_header, &context) {
+                Ok(auth_header) => {
+                    return Some(auth_header);
+                }
+                Err(_) => {
+                    return None;
+                }
+            }
+        }
+        Err(_) => {
+            return None;
+        }
+    }
+}
+
+async fn make_rpc_request(body: &Value,
+                          uri: &String,
+                          username: &String,
+                          password: &String,
+                          auth_header: Option<AuthorizationHeader>) -> Option<Value> {
     let client = Client::new();
-    let result = client.post(uri)
-        .body(body.to_string())
-        .send()
-        .await;
+    let mut builder = client.post(uri)
+        .body(body.to_string());
+    if !auth_header.is_none() {
+        builder = builder.header(AUTH_HEADER_NAME, auth_header.unwrap())
+    }
+    let result = builder.send().await;
     if result.is_ok() {
         let response = result.ok().unwrap();
-        if response.status() == StatusCode::UNAUTHORIZED {
-            let resp_headers = response.headers();
-            println!("{:?}", resp_headers);
+        if response.status() == StatusCode::OK {
+            let resp_str = from_utf8(response.bytes().as_bytes()).unwrap_or(MISSING_STRING);
+            if resp_str.eq(MISSING_STRING) || resp_str.is_empty() {
+                info!("could not parse RPC response");
+                return None;
+            }
+            let default = json!(MISSING_STRING);
+            let parsed_val = serde_json::from_str(resp_str).unwrap_or(default);
+            if parsed_val.as_str().unwrap_or(MISSING_STRING).eq(MISSING_STRING) {
+                info!("could not parse RPC response");
+                return None;
+            }
+            return Some(parsed_val);
+        } else if response.status() == StatusCode::UNAUTHORIZED {
+            if auth_header.is_some() {
+                info!("already had one unauthorized RPC response");
+                return None;
+            }
+            let mut context = AuthContext::new_post(username, password, uri, Some(body));
+            match parse_www_auth_header(response, context) {
+                Some(auth_header) => {
+                    return make_rpc_request(body, uri, username, password, Some(auth_header));
+                }
+                None => {
+                    return None;
+                }
+            }
         } else {
             info!("could not make RPC request");
             return None;
@@ -39,7 +98,6 @@ async fn make_rpc_request(body: Value, uri: &String, username: &String, password
         info!("RPC call failed with error: {}", result.err().unwrap().to_string());
         return None;
     }
-    return Some(json!({}));
 }
 
 pub async fn get_latest_block_template(config: &Config) -> Option<BlockTemplate> {
@@ -53,7 +111,7 @@ pub async fn get_latest_block_template(config: &Config) -> Option<BlockTemplate>
         }
     });
 
-    let response = make_daemon_rpc_request(config, body).await;
+    let response = make_daemon_rpc_request(config, &body).await;
     if response.is_none() {
         warn!("could not get latest block template");
         return None;
@@ -73,7 +131,7 @@ pub async fn submit_block(config: &Config, block_hex: &str) -> Option<Value> {
         "method": "submit_block",
         "params": [block_hex]
     });
-    return make_daemon_rpc_request(config, body).await;
+    return make_daemon_rpc_request(config, &body).await;
 }
 
 pub async fn get_unlocked_balance(config: &Config) -> Option<u64> {
@@ -85,7 +143,7 @@ pub async fn get_unlocked_balance(config: &Config) -> Option<u64> {
             "account_index": 0
         }
     });
-    let req_result = make_wallet_rpc_request(config, body).await;
+    let req_result = make_wallet_rpc_request(config, &body).await;
     if req_result.is_none() {
         info!("could not get RPC balance result");
         return None;
@@ -129,7 +187,7 @@ pub async fn submit_transfers(config: &Config,
             "new_algorithm": true
         }
     });
-    let request_result_opt = make_wallet_rpc_request(config, body).await;
+    let request_result_opt = make_wallet_rpc_request(config, &body).await;
     if request_result_opt.is_none() {
         warn!("could not make submit_block RPC request");
         return None;
